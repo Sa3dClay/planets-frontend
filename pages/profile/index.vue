2234fa570dc9c87ebf5bed1b30155c127ace4bb6
Sa3dClay <sa3d.clay@gmail.com>
2021-11-05 17:49:02 +0200

    profile view

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -5,7 +5,35 @@
       <span>{{ user.planet == 'saturn' ? 'الزوحلي' : 'المريخي' }}</span>
     </h1>
 
-    <v-btn color="indigo" dark to="profile/update">تعديل بياناتي</v-btn>
+    <v-card class="my-4">
+      <v-row justify="center" align="center">
+        <v-col cols="10" sm="4">
+          <div v-if="user.planet == 'saturn'">
+            <v-img
+              :src="require(`~/assets/img/saturn.png`)"
+            ></v-img>
+          </div>
+
+          <div v-else>
+            <v-img
+              :src="require(`~/assets/img/jupiter.png`)"
+            ></v-img>
+          </div>
+        </v-col>
+
+        <v-col cols="10" sm="6">
+          <v-card-title class="d-block">
+            {{ user.name }}
+          </v-card-title>
+
+          <v-card-subtitle>
+            {{ user.email }}
+          </v-card-subtitle>
+        </v-col>
+      </v-row>
+    </v-card>
+
+    <v-btn class="my-4" color="indigo" dark to="profile/update">تعديل بياناتي</v-btn>
   </div>
 </template>
 
