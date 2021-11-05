34bbe5966213fd363d280fe61d76d692d69889ce
Sa3dClay <sa3d.clay@gmail.com>
2021-11-05 16:42:59 +0200

    layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,17 @@
   <v-app>
     <v-main>
       <v-container fluid>
-        <nuxt />
+        <div class="pg">
+          <div class="overlay"></div>
+
+          <div class="pg-content">
+            <v-btn color="success" to="/">الرئيسية</v-btn>
+            
+            <v-btn v-if="authenticated" color="indigo" dark to="/profile">الصفحة الشخصية</v-btn>            
+            
+            <nuxt />
+          </div>
+        </div>
       </v-container>
     </v-main>
   </v-app>
